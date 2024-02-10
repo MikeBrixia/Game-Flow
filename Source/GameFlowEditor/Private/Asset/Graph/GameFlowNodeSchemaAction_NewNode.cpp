93467cda63d7d6b0d2bebfd513517b939b483cac
MikeBrixia <michael.guida@outlook.com>
2024-02-10 13:45:55 +0100

    Added automatic pin connection on node creation

--- Source/GameFlowEditor/Private/Asset/Graph/GameFlowNodeSchemaAction_NewNode.cpp
+++ Source/GameFlowEditor/Private/Asset/Graph/GameFlowNodeSchemaAction_NewNode.cpp
@@ -1,17 +1,36 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿
+// Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Asset/Graph/GameFlowNodeSchemaAction_NewNode.h"
+
+#include "Asset/Graph/GameFlowGraphSchema.h"
 #include "Utils/UGameFlowNodeFactory.h"
 
 UEdGraphNode* FGameFlowNodeSchemaAction_NewNode::PerformAction(UEdGraph* ParentGraph, UEdGraphPin* FromPin,
                                                                const FVector2D Location, bool bSelectNewNode)
 {
 	UGameFlowGraph* GameFlowGraph = CastChecked<UGameFlowGraph>(ParentGraph);
-	
 	// Create a brand new game flow graph node of supplied type.
 	UGameFlowGraphNode* GraphNode = UGameFlowNodeFactory::CreateGraphNode(NodeClass, GameFlowGraph->GameFlowAsset, GameFlowGraph);
-    GraphNode->NodePosX = Location.X;
+	GraphNode->NodePosX = Location.X;
 	GraphNode->NodePosY = Location.Y;
+	
+	// Is the context menu been created after a pin drag action?
+	if(FromPin != nullptr)
+	{
+		UEdGraphPin* NewNodeTargetPin = nullptr;
+        if(FromPin->Direction == EGPD_Input)
+        {
+	        NewNodeTargetPin = GraphNode->GetPinWithDirectionAt(0, EGPD_Output);
+        }
+		else if(FromPin->Direction == EGPD_Output)
+		{
+			NewNodeTargetPin = GraphNode->GetPinWithDirectionAt(0, EGPD_Input);
+		}
+		const UGameFlowGraphSchema* GameFlowGraphSchema = CastChecked<UGameFlowGraphSchema>(GameFlowGraph->GetSchema());
+		// If true create an automatic connection with the new node first pin.
+		GameFlowGraphSchema->TryCreateConnection(FromPin, NewNodeTargetPin);
+	}
 	
 	return GraphNode;
 }
