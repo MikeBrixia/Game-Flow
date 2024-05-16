289f67a4d21fe6efbf28f2fac939ca7c94205a37
MikeBrixia <michael.guida@outlook.com>
2024-05-16 13:04:14 +0200

    Added pin handle structure

---
+++ Source/GameFlow/Private/Nodes/PinHandle.cpp
@@ -0,0 +1,46 @@
+﻿#include "Nodes/PinHandle.h"
+
+FPinHandle::FPinHandle()
+{
+	PinName = EName::None;
+}
+
+FPinHandle::FPinHandle(const FPinHandle& Other)
+{
+	this->Connections = Other.Connections;
+	this->PinName = Other.PinName;
+}
+
+FPinHandle::FPinHandle(TMap<FName, UGameFlowNode*> Connections, FName PinName)
+{
+	this->Connections = Connections;
+	this->PinName = PinName;
+}
+
+void FPinHandle::CreateConnection(FPinHandle& OtherPinHandle)
+{
+	// Do we have the permission to connect this two handles?
+	if(CanCreateConnection(OtherPinHandle))
+	{
+		// First create a connection from this handle to the other...
+		Connections.Add(OtherPinHandle.PinName, OtherPinHandle.PinOwner);
+		// ... then create a connection in the opposite direction.
+		OtherPinHandle.Connections.Add(PinName, PinOwner);
+	}
+}
+
+bool FPinHandle::IsValidHandle() const
+{
+	// A valid pin name should not be 'None' or whitespaces only.
+	const bool bIsValidName = !PinName.IsNone() && PinName.IsValid()
+	       && !PinName.ToString().IsEmpty();
+	return bIsValidName && PinOwner != nullptr;
+}
+
+bool FPinHandle::CanCreateConnection(const FPinHandle& OtherPinHandle) const
+{
+	const bool bValidHandles = IsValidHandle() && OtherPinHandle.IsValidHandle();
+	const bool bValidOwners = PinOwner != nullptr && OtherPinHandle.PinOwner != nullptr;
+	const bool bRecursiveConnection = PinOwner == OtherPinHandle.PinOwner;
+	return bValidHandles && bValidOwners && !bRecursiveConnection;
+}
---
+++ Source/GameFlow/Public/Nodes/PinHandle.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+class UGameFlowNode;
+
+/** Utility structure used to handle Game Flow I/O pins. */
+USTRUCT(BlueprintType)
+struct GAMEFLOW_API FPinHandle
+{
+	GENERATED_USTRUCT_BODY()
+	
+	FPinHandle();
+	FPinHandle(const FPinHandle& Other);
+	FPinHandle(TMap<FName, UGameFlowNode*> Connections, FName PinName);
+	
+	/** All the connections held by this pin. */
+	UPROPERTY(VisibleAnywhere)
+	TMap<FName, UGameFlowNode*> Connections;
+
+	/** The name of the handled pin. */
+	UPROPERTY(VisibleAnywhere)
+	FName PinName;
+
+	/** The node who owns this pin. */
+    UPROPERTY()
+    TObjectPtr<UGameFlowNode> PinOwner;
+
+#if WITH_EDITOR
+	/** Create a connection between this handle and another pin handle. */
+    void CreateConnection(FPinHandle& OtherPinHandle);
+
+	/** Check if this pin handle is valid and ready to be used. */
+    bool IsValidHandle() const;
+    bool CanCreateConnection(const FPinHandle& OtherPinHandle) const;
+#endif 
+};
